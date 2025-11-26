8fb3b02525588c374d3bfb9bb30304c87e9807ff
reagan luyinda <luyindareagan@gmail.com>
2025-11-26 20:56:40 +0300

    3.3 phonebook backend step 3, create route to fetch a single phonebook entry

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ app.get("/api/info", (request, response) => {
   );
 });
 
+//GET request to fetch a person by ID
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
